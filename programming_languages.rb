8fa26f7e548c785be386f97eb6315b138e0b4ba5
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-15 12:53:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,12 +3,6 @@ def reformat_languages(languages)
 
 languages.each do |style, desc| 
   desc.each do |lang, describe| 
-  if style==:oo 
- new_hash<< new_hash[lang][:style]=[:oo]
-  
-  if style==:functional 
-     new_hash<< new_hash[lang][:style]=[:functional]
-    end
-  end
-   new_hash  
-end	
+
+new_hash[lang]
+ 
